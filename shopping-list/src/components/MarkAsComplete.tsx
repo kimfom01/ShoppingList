8283315ba3a9ce2fa41c779ec27b5ccf93805501
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-15 02:46:33 +0300

    Implement mark as complete component

---
+++ shopping-list/src/components/MarkAsComplete.tsx
@@ -0,0 +1,23 @@
+import { Button } from "./Button";
+
+interface Prop {
+  setPicked: React.Dispatch<React.SetStateAction<boolean>>;
+  picked: boolean;
+  itemId: string;
+}
+
+export const MarkAsComplete = ({ setPicked, picked, itemId }: Prop) => {
+  const handleClick = () => {
+    fetch(`${import.meta.env.VITE_API_ROOT}/${itemId}`, {
+      method: "patch",
+    })
+      .then((res) => {
+        if (res.status === 204) {
+          setPicked(!picked);
+        }
+      })
+      .catch((err) => console.error(err.message));
+  };
+
+  return <Button onClick={handleClick} icon={"✅"} />;
+};
