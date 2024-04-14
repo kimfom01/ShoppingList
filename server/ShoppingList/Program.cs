a327c6e30390599641d8ff2b6bc1dc59265179b3
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-14 17:39:59 +0300

    register services and setup

--- server/ShoppingList/Program.cs
+++ server/ShoppingList/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingList.Data;
+using ShoppingList.Repositories;
+using ShoppingList.Services;
+using ShoppingList.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ShoppingListDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ShoppingListDb"));
+    // options.UseInMemoryDatabase("ShoppingListDb");
+});
+builder.Services.AddScoped<IShoppingListRepository, ShoppingListRepository>();
+builder.Services.AddScoped<IShoppingListService, ShoppingListService>();
 
 var app = builder.Build();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 if (app.Environment.IsDevelopment())
 {
@@ -15,5 +30,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+app.Setup(builder.Environment);
 
 app.Run();
