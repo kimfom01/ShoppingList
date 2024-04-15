4b017e0a4b448bcd5c60c32f68030d657c5c5792
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-04-15 03:00:12 +0300

    Add cors and swagger documentation config

--- server/ShoppingList/Program.cs
+++ server/ShoppingList/Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ShoppingList.Data;
 using ShoppingList.Repositories;
 using ShoppingList.Services;
@@ -6,9 +8,43 @@ using ShoppingList.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string corsPolicy = "corspolicy";
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Shopping List API",
+            Description = "A Web API for for the Shopping List Web App",
+            Contact = new OpenApiContact
+            {
+                Name = "Kim Fom",
+                Email = "kimfom01@gmail.com",
+                Url = new Uri("https://kimfom.space")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "MIT Licence",
+                Url = new Uri("https://opensource.org/license/mit/")
+            }
+        }
+    );
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: corsPolicy,
+        policy => policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod()
+    );
+});
 builder.Services.AddDbContext<ShoppingListDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("ShoppingListDb"));
@@ -28,6 +64,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsPolicy);
 
 app.MapControllers();
 
